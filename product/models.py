c714a65ebb541412cb94abb6580d05e89e198af7
Emran bolouri <Emransix@gmail.com>
2023-06-04 22:03:31 +0330

    add product app

---
+++ product/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    short_desc = models.TextField()
+    desc = models.TextField()
+    thumbnail_1 = models.ImageField(upload_to='images/products', blank=True, null=True)
+    thumbnail_2 = models.ImageField(upload_to='images/products', blank=True, null=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    thumbnail = models.ImageField(upload_to='images/categories', blank=True, null=True)
+    short_desc = models.TextField()
+
+
+    def __str__(self):
+        return self.name
--- turbin/urls.py
+++ turbin/urls.py
@@ -22,7 +22,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('blog', include('blog.urls'))
+    path('blog', include('blog.urls')),
+    path('product', include('product.urls')),
 
 ]
 
