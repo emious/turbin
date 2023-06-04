5513f885453269f3225fee35e8bb529fd458c698
emious <emransix@gmail.com>
2023-06-04 18:01:04 +0330

    add Website Setting

--- main/models.py
+++ main/models.py
@@ -1,8 +1,10 @@
+import os
+
 from django.core.exceptions import ValidationError
 from django.db import models
 
+from django.conf import settings
 
-# Create your models here.
 
 class Slider(models.Model):
     class Meta:
@@ -19,9 +21,22 @@ class HeaderMenu:
     link = models.SlugField(),
 
 
-class WebsiteSetting(models.Model):
-    phone_number = models.CharField(max_length=12)
 
+class WebsiteSetting(models.Model):
+
+    phone_number = models.CharField(max_length=12, default='', blank=True)
+    email = models.CharField(max_length=30, default='', blank=True)
+    address = models.CharField(max_length=50, default='', blank=True)
+    work_time = models.CharField(max_length=30, default='', blank=True)
+    about = models.TextField(default='', blank=True)
+    favicon = models.ImageField(upload_to='images/template/', blank=True)
+    footer_logo = models.ImageField(upload_to='images/template/', blank=True)
+    logo = models.ImageField(upload_to='images/template/', blank=True)
+    logo_small = models.ImageField(upload_to='images/template/', blank=True,)
+
+
+
+    
     def save(self, *args, **kwargs):
         if not self.pk and WebsiteSetting.objects.exists():
             # if you'll not check for self.pk
